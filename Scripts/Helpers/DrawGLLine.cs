6a8757be12a876cbaf4e7ceb9b6529ac1c490590
mika <unitycoder@users.noreply.github.com>
2016-07-03 16:54:16 +0800

    Update DrawGLLine.cs

--- Scripts/Helpers/DrawGLLine.cs
+++ Scripts/Helpers/DrawGLLine.cs
@@ -9,29 +9,31 @@ public class DrawGLLine : MonoBehaviour
 
     Material lineMaterial;
 
+    void Awake()
+    {
+        // must be called before trying to draw lines..
+        CreateLineMaterial();
+    }
+
     void CreateLineMaterial()
     {
-        if (!lineMaterial)
-        {
-            // Unity has a built-in shader that is useful for drawing simple colored things
-            var shader = Shader.Find("Hidden/Internal-Colored");
-            lineMaterial = new Material(shader);
-            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
-            // Turn on alpha blending
-            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            // Turn backface culling off
-            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
-            // Turn off depth writes
-            lineMaterial.SetInt("_ZWrite", 0);
-        }
+        // Unity has a built-in shader that is useful for drawing simple colored things
+        var shader = Shader.Find("Hidden/Internal-Colored");
+        lineMaterial = new Material(shader);
+        lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+        // Turn on alpha blending
+        lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        // Turn backface culling off
+        lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+        // Turn off depth writes
+        lineMaterial.SetInt("_ZWrite", 0);
     }
 
 
     // cannot call this on update, line wont be visible then.. and if used OnPostRender() thats works when attached to camera only
     void OnRenderObject()
     {
-        CreateLineMaterial();
         lineMaterial.SetPass(0);
 
         GL.PushMatrix();
