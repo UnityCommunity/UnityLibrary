441a45726793a4f6120f21d2a6f57f4409bc506d
mika <unitycoder@users.noreply.github.com>
2016-08-03 16:18:04 +0800

    Create MeteorRing.cs

---
+++ Scripts/Misc/MeteorRing.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+// Usage: Attach to gameobject (enable gizmos to see Debug.DrawRay())
+// reference: http://forum.unity3d.com/threads/procedural-generation-in-a-specific-shape-question.421659/
+
+public class MeteorRing : MonoBehaviour
+{
+    public int totalCount = 5000;
+    public float ringRadius = 10;
+    public float ringHeight = 1;
+
+    void Start()
+    {
+        for (int i = 0; i < totalCount; i++)
+        {
+            // outer ring
+            float angle = i * (Mathf.PI *2) / totalCount;
+            var x = Mathf.Sin(angle) * ringRadius;
+            var y = Mathf.Cos(angle) * ringRadius;
+            var pos = new Vector3(x, 0, y);
+
+            // spread within outer ring
+            pos += Random.insideUnitSphere * ringHeight;
+
+            // draw
+            Debug.DrawRay(pos, Vector3.up * 0.05f, Color.yellow, 100);
+        }
+    }
+}
