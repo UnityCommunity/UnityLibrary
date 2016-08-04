aac7a5b3b9091af067a8fc136c4d39448bf7e0e6
mika <unitycoder@users.noreply.github.com>
2016-08-04 09:49:20 +0800

    Create ManualCompiler.bat

---
+++ External/Compiler/ManualCompiler.bat
@@ -0,0 +1,14 @@
+@echo off
+REM *** THIS IS NOT WORKING YET ***
+REM UnityManualCompiler v0.1
+REM Usage: Rename ..Unity\Editor\Data\Mono\bin\mono.exe into xmono.exe (to make auto-compilation fail)
+REM Set your own Unity installation folder, MonoPath, TempFile, CurrentDirectory
+
+C:
+cd "C:\Program Files\Unity\Editor\Data\Mono\bin\"
+
+"C:\Program Files\Unity\Editor\Data\Mono\bin\xmono.exe" CommandLine="C:\Program Files\Unity\Editor\Data\Mono\lib\mono\unity\smcs.exe  @Temp/UnityTempFile-asdasdasdasdasd", CurrentDirectory="D:/Folder/ProjectA/Assets/.."
+
+REM - TODO Fix missing dll error..
+
+pause
