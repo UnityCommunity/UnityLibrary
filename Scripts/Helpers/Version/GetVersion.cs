4d7087818b328acce770007e9e2b9ba7e885dd98
mika <unitycoder@users.noreply.github.com>
2016-10-16 17:21:26 +0800

    Create GetVersion.cs

---
+++ Scripts/Helpers/Version/GetVersion.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// Usage: attach this to UI Text component
+// it displays current version number from Ios/Android player settings with Application.version
+
+public class GetVersion : MonoBehaviour
+{
+    void Awake()
+    {
+        var t = GetComponent<Text>();
+        if (t!=null)
+        {
+            t.text = "v"+Application.version;
+        }
+    }
+}
