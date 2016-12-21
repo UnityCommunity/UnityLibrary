b6dd4581094f557a994f3bcd8ef68833f9ccb28c
mika <unitycoder@users.noreply.github.com>
2016-12-21 17:52:43 +0800

    Create DrawCameraFrustumGizmos.cs

---
+++ Scripts/Editor/Gizmos/DrawCameraFrustumGizmos.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEditor;
+
+// draws camera frustum lines with Gizmo lines (when camera is not selected)
+// Usage: add this script into Editor/ folder on your project
+// WARNING: cam.transform.position does not work, DrawFrustum ignores the value, unity bug?
+
+static class DrawCameraFrustumGizmos
+{
+    [DrawGizmo(GizmoType.NotInSelectionHierarchy)]// | GizmoType.Active)]
+    static void DrawGizmoForMyScript(Camera cam,GizmoType gizmoType)
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawFrustum(cam.transform.position, cam.fieldOfView,cam.farClipPlane, cam.nearClipPlane,cam.aspect);
+    }
+}
