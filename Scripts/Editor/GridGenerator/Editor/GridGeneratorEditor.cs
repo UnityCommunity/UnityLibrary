141750e0fe4b9031f1a86fe890c5c82b3abcd664
Sacristan <girts.kesteris@gmail.com>
2017-07-04 16:59:43 +0300

    added grid generator

---
+++ Scripts/Editor/GridGenerator/Editor/GridGeneratorEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Linq;
+
+[CustomEditor(typeof(GridGenerator))]
+public class GridGeneratorEditor : Editor
+{
+    GridGenerator t;
+
+    private void OnEnable()
+    {
+        t = (GridGenerator) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Cleanup"))
+        {
+            CleanUp();
+        }
+
+        if (GUILayout.Button("Generate Grid"))
+        {
+            CleanUp();
+            GenerateGrid();
+        }
+    }
+
+    private void CleanUp()
+    {
+        List<Transform> tempList = t.transform.Cast<Transform>().ToList();
+        tempList.ForEach(x => DestroyImmediate(x.gameObject));
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < t.SizeX; x++)
+        {
+            for (int y = 0; y < t.SizeY; y++)
+            {
+                Vector3 localOffset = new Vector3(
+                    t.Offset.x * x,
+                    0,
+                    t.Offset.y * y
+                );
+
+                GameObject spawnedObject = Instantiate(t.PrefabToPlace);
+
+                spawnedObject.transform.SetParent(t.transform);
+                spawnedObject.transform.localPosition = localOffset;
+
+                spawnedObject.name = string.Format("{0} ({1},{2})", t.PrefabToPlace.name, x, y);
+            }
+        }
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+    }
+
+}
---
+++ Scripts/Editor/GridGenerator/GridGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabToPlace;
+
+    [SerializeField]
+    private uint sizeX = 2;
+
+    [SerializeField]
+    private uint sizeY = 2;
+
+    [SerializeField]
+    private Vector2 offset = Vector2.one;
+
+    public GameObject PrefabToPlace { get { return prefabToPlace; } }
+    public Vector2 Offset { get { return offset; } }
+
+    public uint SizeX { get { return sizeX; } }
+    public uint SizeY { get { return sizeY; } }
+}
