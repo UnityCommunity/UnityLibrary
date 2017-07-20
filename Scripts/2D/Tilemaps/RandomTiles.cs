e21a5842a30b39ef44c8846120e50dbef4aed48b
mika <unitycoder@users.noreply.github.com>
2017-07-20 23:33:00 +0300

    Update RandomTiles.cs #15

--- Scripts/2D/Tilemaps/RandomTiles.cs
+++ Scripts/2D/Tilemaps/RandomTiles.cs
@@ -1,33 +1,71 @@
-// Requires: Unity 5.5.0a1 or later
-// Fills tilemap with random tiles
-// Usage: Attach this script to Tilemap layer, assign tiles, hit play
+// tested with unity version: 2017.2.0b4
+// info: Fills tilemap with random tiles
+// usage: Attach this script to empty gameobject, assign some tiles, then press play
 
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class RandomTiles : MonoBehaviour
+namespace UnityLibary
 {
-    public Tile[] tiles;
-
-    void Start()
+    public class RandomTiles : MonoBehaviour
     {
-        RandomTileMap();
-    }
+        public int width = 32;
+        public int height = 32;
 
-    void RandomTileMap()
-    {
-        TileMap map = GetComponent<TileMap>();
+        public Tile[] tiles;
 
-        int sizeX = 32;
-        int sizeY = 16;
-
-        for (int x = 0; x < sizeX; x++)
+        void Start()
         {
-            for (int y = 0; y < sizeY; y++)
+            RandomTileMap();
+        }
+
+        void RandomTileMap()
+        {
+            // validation
+            if (tiles == null || tiles.Length < 1)
             {
-                var tilePos = new Vector3Int(x, y, 0);
-                map.SetTile(tilePos, tiles[Random.Range(0, tiles.Length)]);
+                Debug.LogError("Tiles not assigned", gameObject);
+                return;
+            }
+
+            var parent = transform.parent;
+            if (parent == null)
+            {
+                var go = new GameObject("grid");
+                go.AddComponent<Grid>();
+                transform.SetParent(go.transform);
+            } else
+            {
+                if (parent.GetComponent<Grid>() == null)
+                {
+                    parent.gameObject.AddComponent<Grid>();
+                }
+            }
+
+            TilemapRenderer tr = GetComponent<TilemapRenderer>();
+            if (tr == null)
+            {
+                tr = gameObject.AddComponent<TilemapRenderer>();
+            }
+
+            Tilemap map = GetComponent<Tilemap>();
+            if (map == null)
+            {
+                map = gameObject.AddComponent<Tilemap>();
+            }
+
+
+            // random map generation
+            Vector3Int tilePos = Vector3Int.zero;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    tilePos.x = x;
+                    tilePos.y = y;
+                    map.SetTile(tilePos, tiles[Random.Range(0, tiles.Length)]);
+                }
             }
         }
     }
-
 }
