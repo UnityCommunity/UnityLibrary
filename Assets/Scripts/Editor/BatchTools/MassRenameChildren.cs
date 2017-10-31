61d4d437242614b9044ff046ea073ed13cf90d27
mika <unitycoder@users.noreply.github.com>
2017-10-31 12:36:15 +0800

    Update MassRenameChildren.cs

--- Assets/Scripts/Editor/BatchTools/MassRenameChildren.cs
+++ Assets/Scripts/Editor/BatchTools/MassRenameChildren.cs
@@ -10,6 +10,8 @@ namespace UnityLibrary
     {
         public string findString = "";
         public string replaceWith = "";
+        // if set false: would replace "Hand" inside "RightHandRig", if set true: would replace "Hand" only if name starts with "Hand" like "HandRigWasd"
+        public bool onlyIfStartsWithFindString = true; 
 
         [MenuItem("GameObject/Mass Rename Children")]
         static void CreateWizard()
@@ -33,12 +35,27 @@ namespace UnityLibrary
                 // skip self (selection root)
                 if (child != Selection.activeTransform)
                 {
-                    var newName = child.name.Replace(findString, replaceWith);
-                    Debug.LogFormat("Before: {0} | After: {1}", child.name, newName);
-                    child.name = newName;
+                    string newName = child.name;
+                    if (onlyIfStartsWithFindString == true)
+                    {
+                        // string starts with our search string
+                        if (child.name.IndexOf(findString) == 0)
+                        {
+                            newName = child.name.Replace(findString, replaceWith);
+                        }
+                    } else // replace anywhere in target string
+                    {
+                        newName = child.name.Replace(findString, replaceWith);
+                    }
+
+                    // if would have any changes to name, print out and change
+                    if (child.name != newName)
+                    {
+                        Debug.LogFormat("Before: {0} | After: {1}", child.name, newName);
+                        child.name = newName;
+                    }
                 }
             }
         }
-        
     }
 }
