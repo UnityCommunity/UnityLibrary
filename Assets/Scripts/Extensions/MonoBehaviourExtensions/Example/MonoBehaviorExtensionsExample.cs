57b272041a5308bfc62c4c355a3273f69805db5d
Sacristan <girts.kesteris@gmail.com>
2019-01-02 11:23:25 +0200

    add monobehaviour extensions

---
+++ Assets/Scripts/Extensions/MonoBehaviourExtensions/Example/MonoBehaviorExtensionsExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Sacristan.Utils.Extensions;
+
+public class MonoBehaviorExtensionsTest : MonoBehaviour
+{
+    private void Start()
+    {
+        this.InvokeRepeatingSafe(Tick, 1f, 1f);
+        this.InvokeSafe(Tock, 2f);
+    }
+
+    private void Tick()
+    {
+        Debug.Log("Tick");
+    }
+
+    private void Tock()
+    {
+        Debug.Log("Tock");
+    }
+}
---
+++ Assets/Scripts/Extensions/MonoBehaviourExtensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Sacristan.Utils.Extensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static void InvokeSafe(this MonoBehaviour behavior, System.Action method, float delayInSeconds)
+        {
+            behavior.StartCoroutine(InvokeSafeRoutine(method, delayInSeconds));
+        }
+
+        public static void InvokeRepeatingSafe(this MonoBehaviour behavior, System.Action method, float delayInSeconds, float repeatRateInSeconds)
+        {
+            behavior.StartCoroutine(InvokeSafeRepeatingRoutine(method, delayInSeconds, repeatRateInSeconds));
+        }
+
+        private static IEnumerator InvokeSafeRepeatingRoutine(System.Action method, float delayInSeconds, float repeatRateInSeconds)
+        {
+            yield return new WaitForSeconds(delayInSeconds);
+
+            while (true)
+            {
+                if (method != null) method.Invoke();
+                yield return new WaitForSeconds(repeatRateInSeconds);
+            }
+        }
+
+        private static IEnumerator InvokeSafeRoutine(System.Action method, float delayInSeconds)
+        {
+            yield return new WaitForSeconds(delayInSeconds);
+            if (method != null) method.Invoke();
+        }
+    }
+}
