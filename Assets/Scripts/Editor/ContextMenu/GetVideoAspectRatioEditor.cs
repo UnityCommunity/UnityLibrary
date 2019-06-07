1543589205584b248b0c2051a4f7337d8527b4b3
mika <unitycoder@users.noreply.github.com>
2019-06-07 11:55:23 +0300

    Create GetVideoAspectRatioEditor.cs

---
+++ Assets/Scripts/Editor/ContextMenu/GetVideoAspectRatioEditor.cs
@@ -0,0 +1,31 @@
+// Custom Contect menu for VideoPlayer component
+// used for scaling quad mesh transform localscale.y to match videoplayer aspect ratio
+
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Video;
+
+namespace UnityLibrary
+{
+    public class GetVideoAspectRatioEditor : MonoBehaviour
+    {
+        [MenuItem("CONTEXT/VideoPlayer/Get Aspect Ratio for Mesh")]
+        static void DoubleMass(MenuCommand command)
+        {
+            // get aspect ratio
+            VideoPlayer v = (VideoPlayer)command.context;
+            if (v.clip == null)
+            {
+                Debug.LogError("No videoclip assigned..");
+                return;
+            }
+            float aspectRatioY = v.height / (float)v.width;
+
+            // scale mesh
+            Vector3 scale = v.transform.localScale;
+            // fix only height
+            scale.y *= aspectRatioY;
+            v.transform.localScale = scale;
+        }
+    }
+}
