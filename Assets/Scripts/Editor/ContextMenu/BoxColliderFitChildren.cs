fcdbc8c57e9c1094a25683b8e78169861b14c863
mika <unitycoder@users.noreply.github.com>
2020-06-11 13:10:10 +0300

    Create BoxColliderFitChildren.cs

---
+++ Assets/Scripts/Editor/ContextMenu/BoxColliderFitChildren.cs
@@ -0,0 +1,45 @@
+// Adjust Box Collider to fit child meshes inside
+// usage: You have empty parent transform, with child meshes inside, add box collider to parent then use this
+
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityLibrary
+{
+    public class BoxColliderFitChildren : MonoBehaviour
+    {
+        [MenuItem("CONTEXT/BoxCollider/Fit to Children")]
+        static void FixSize(MenuCommand command)
+        {
+            BoxCollider col = (BoxCollider)command.context;
+
+            // record undo
+            Undo.RecordObject(col.transform, "Fit Box Collider To Children");
+
+            // get child mesh bounds
+            var b = GetRecursiveMeshBounds(col.gameObject);
+
+            // set collider local center and size
+            col.center = col.transform.root.InverseTransformVector(b.center) - col.transform.position;
+            col.size = b.size;
+        }
+
+        public static Bounds GetRecursiveMeshBounds(GameObject go)
+        {
+            var r = go.GetComponentsInChildren<Renderer>();
+            if (r.Length > 0)
+            {
+                var b = r[0].bounds;
+                for (int i = 1; i < r.Length; i++)
+                {
+                    b.Encapsulate(r[i].bounds);
+                }
+                return b;
+            }
+            else // TODO no renderers
+            {
+                return new Bounds(Vector3.one, Vector3.one);
+            }
+        }
+    }
+}
