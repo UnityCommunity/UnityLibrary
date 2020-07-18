c579bf50ea0dea8f015d6522ac8e46667f51a254
pkunjam <punit17100@iiitnr.edu.in>
2020-07-18 12:31:12 +0530

    script to display webcam texture into a gameobject

---
+++ Assets/Scripts/Texture/WebCamDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// usage: attach this script to any gameobject in which you want to render the webcam display
+// create a material, use Unlit/Texture as a shader and place it to the gameobject's material placeholder in the mesh renderer component
+
+namespace UnityLibrary
+{
+    public class WebCamDisplay : MonoBehaviour
+    {
+        void Start()
+        {
+            WebCamTexture webCam = new WebCamTexture();
+            webCam.deviceName = WebCamTexture.devices[0].name;
+            this.GetComponent<MeshRenderer>().material.mainTexture = webCam;
+            webCam.Play();
+        }
+    }
+}
+
