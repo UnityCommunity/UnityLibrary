68e74b17266b2473be6f109ebc08a4cace9948de
LesserKnownThings <ursu.marius.laurentiu@gmail.com>
2020-09-11 13:29:49 -0400

    Adding Object Pooling

---
+++ Assets/Scripts/ObjectPooling/PoolObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityHelper.Pooling
+{
+    [Serializable]
+    public class PoolObject
+    {
+        /// <summary>
+        /// The gameobject that you want to spawn
+        /// </summary>
+        public GameObject obj_to_pool;
+        /// <summary>
+        /// The amount of objects that you want to spawn
+        /// </summary>
+        public int amount_to_pool;
+    }
+}
