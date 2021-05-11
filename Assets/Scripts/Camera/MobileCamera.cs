ac3ae833ee4b1636c521ca01b7e2d0c452fe37e7
mika <unitycoder@users.noreply.github.com>
2021-05-11 08:07:06 +0300

    add MobileCamera.cs

---
+++ Assets/Scripts/Camera/MobileCamera.cs
@@ -0,0 +1,135 @@
+// https://forum.unity.com/threads/mobile-touch-to-orbit-pan-and-zoom-camera-without-fix-target-in-one-script.522607/#post-3531342
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public float distance = 5.0f;
+    public float maxDistance = 20;
+    public float minDistance = .6f;
+    public float xSpeed = 5.0f;
+    public float ySpeed = 5.0f;
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+    public float zoomRate = 10.0f;
+    public float panSpeed = 0.3f;
+    public float zoomDampening = 5.0f;
+
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+    private float currentDistance;
+    private float desiredDistance;
+    private Quaternion currentRotation;
+    private Quaternion desiredRotation;
+    private Quaternion rotation;
+    private Vector3 position;
+
+    private Vector3 FirstPosition;
+    private Vector3 SecondPosition;
+    private Vector3 delta;
+    private Vector3 lastOffset;
+    private Vector3 lastOffsettemp;
+    //private Vector3 CameraPosition;
+    //private Vector3 Targetposition;
+    //private Vector3 MoveDistance;
+
+    public float offsetSpeed = 0.03f;
+
+    void Start() { Init(); }
+    void OnEnable() { Init(); }
+
+    public void Init()
+    {
+        //If there is no target, create a temporary target at 'distance' from the cameras current viewpoint
+        if (!target)
+        {
+            GameObject go = new GameObject("Cam Target");
+            go.transform.position = transform.position + (transform.forward * distance);
+            target = go.transform;
+        }
+
+        distance = Vector3.Distance(transform.position, target.position);
+        currentDistance = distance;
+        desiredDistance = distance;
+
+        //be sure to grab the current rotations as starting points.
+        position = transform.position;
+        rotation = transform.rotation;
+        currentRotation = transform.rotation;
+        desiredRotation = transform.rotation;
+
+        //xDeg = Vector3.Angle(Vector3.right, transform.right);
+        //yDeg = Vector3.Angle(Vector3.up, transform.up);
+
+        xDeg = transform.eulerAngles.y + Vector3.Angle(Vector3.right, transform.right);
+        yDeg = transform.eulerAngles.x + Vector3.Angle(Vector3.up, transform.up);
+
+    }
+
+    void LateUpdate()
+    {
+        // If Control and Alt and Middle button? ZOOM!
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+            Vector2 touchZeroPreviousPosition = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePreviousPosition = touchOne.position - touchOne.deltaPosition;
+            float prevTouchDeltaMag = (touchZeroPreviousPosition - touchOnePreviousPosition).magnitude;
+            float TouchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+            float deltaMagDiff = prevTouchDeltaMag - TouchDeltaMag;
+            desiredDistance += deltaMagDiff * Time.deltaTime * zoomRate * 0.0025f * Mathf.Abs(desiredDistance);
+        }
+
+        // If middle mouse and left alt are selected? ORBIT
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchposition = Input.GetTouch(0).deltaPosition;
+            xDeg += touchposition.x * xSpeed * 0.002f;
+            yDeg -= touchposition.y * ySpeed * 0.002f;
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+
+        }
+
+        desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
+        currentRotation = transform.rotation;
+        rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
+        transform.rotation = rotation;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            FirstPosition = Input.mousePosition;
+            lastOffset = targetOffset;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            SecondPosition = Input.mousePosition;
+            delta = SecondPosition - FirstPosition;
+            targetOffset = lastOffset + transform.right * delta.x * offsetSpeed + transform.up * delta.y * offsetSpeed;
+
+        }
+
+        ////////Orbit Position
+        // affect the desired Zoom distance if we roll the scrollwheel
+        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
+        currentDistance = Mathf.Lerp(currentDistance, desiredDistance, Time.deltaTime * zoomDampening);
+        position = target.position - (rotation * Vector3.forward * currentDistance);
+        position = position - targetOffset;
+        transform.position = position;
+    }
+
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
+
