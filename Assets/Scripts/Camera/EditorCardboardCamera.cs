c068ce60b0953a20998328d4f8efb8426a5ed0ed
mika <unitycoder@users.noreply.github.com>
2021-10-05 09:53:16 +0300

    add EditorCardboardCamera script

---
+++ Assets/Scripts/Camera/EditorCardboardCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// editor only Google Carboard VR cam simulation with left alt + mouse
+
+public class EditorCardboardCamera : MonoBehaviour
+{
+#if UNITY_EDITOR
+    Vector2 rotation = Vector2.zero;
+    public float speed = 3;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            rotation.y += Input.GetAxis("Mouse X");
+            rotation.x += -Input.GetAxis("Mouse Y");
+            transform.eulerAngles = (Vector2)rotation * speed;
+        }
+    }
+#endif
+}
