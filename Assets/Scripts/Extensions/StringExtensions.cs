4d4b3ee01093ec5bb62815c83b92d2530807d82e
wbm1113 <46951987+wbm1113@users.noreply.github.com>
2022-03-09 21:00:16 -0500

    Create StringExtensions.cs

---
+++ Assets/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Globalization;
+
+namespace UnityLibrary
+{
+    public static class StringExtensions
+    {
+        public static Color ToColor(this string hex) {
+            hex = hex.Replace("0x", "");
+            hex = hex.Replace("#", "");
+
+            byte a = 255;
+            byte r = byte.Parse(hex.Substring(0,2), NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2,2), NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4,2), NumberStyles.HexNumber);
+
+            if (hex.Length == 8)
+                a = byte.Parse(hex.Substring(6,2), NumberStyles.HexNumber);
+
+            return new Color32(r,g,b,a);
+        }
+    }
+}
